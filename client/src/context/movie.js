cd7339ae7f9cccaac6d7e3f133a70d52c7f501f9
Justin Bass <justinbass6036@gmail.com>
2023-10-18 15:34:33 -0400

    Set up UserProvider context for global state. Might not need it later. Set up filter methods to render movies on Home page based off genere and boolean conditonals. Added some css for Home page.

--- client/src/context/movie.js
+++ client/src/context/movie.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Movies from "../components/Movies";
 
 //this gives me global state
 const UserContext = React.createContext()
 
 function UserProvider({ children }) {
     const [ movies, setMovies ] = useState( [] )
-
-
 
     useEffect(() => {
         fetch('/movies')
@@ -16,10 +15,115 @@ function UserProvider({ children }) {
         })
     }, []);
 
+    const randomMovieTrailer = Math.floor(Math.random() * movies.length)
+
+    const popularMovies = movies.filter((movie) => movie.popular ? true : false)
+    const renderPopularMovies = popularMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const newMovies = movies.filter((movie) => movie.new_release? true : false)
+    const renderNewMovies = newMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const trendingMovies = movies.filter((movie) => movie.trending_now ? true : false)
+    const renderTrendingMovies = trendingMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const actionMovies = movies.filter((movie) => movie.genre === 'Action')
+    const renderActionMovies = actionMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const comedyMovies = movies.filter((movie) => movie.genre === 'Comedy')
+    const renderComedyMovies = comedyMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const romanceMovies = movies.filter((movie) => movie.genre === 'Romance')
+    const renderRomanceMovies = romanceMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const dramaMovies = movies.filter((movie) => movie.genre === 'Drama')
+    const renderDramaMovies = dramaMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const thrillerMovies = movies.filter((movie) => movie.genre === 'Thriller')
+    const renderThrillerMovies = thrillerMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const adventureMovies = movies.filter((movie) => movie.genre === 'Adventure')
+    const renderAdventureMovies = adventureMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const crimeMovies = movies.filter((movie) => movie.genre === 'Crime')
+    const renderCrimeMovies = crimeMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+    const horrorMovies = movies.filter((movie) => movie.genre === 'Horror')
+    const renderHorrorMovies = horrorMovies.map((movie) => (
+        <Movies
+        key={ movie.id }
+        movie={ movie }
+        />
+    ))
+
+
+
 
     return(
         <UserContext.Provider
-        value={{ movies }}>
+        value={{
+            movies,
+            randomMovieTrailer,
+            renderPopularMovies,
+            renderNewMovies,
+            renderTrendingMovies,
+            renderActionMovies,
+            renderComedyMovies,
+            renderRomanceMovies,
+            renderDramaMovies,
+            renderThrillerMovies,
+            renderAdventureMovies,
+            renderCrimeMovies,
+            renderHorrorMovies}}>
             {children}
         </UserContext.Provider>
     )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,17 +3,22 @@ import { UserContext } from '../context/movie'
 import Movies from '../components/Movies'
 
 export default function Home() {
-    const { movies } = useContext( UserContext )
+    const {
+        movies,
+        randomMovieTrailer,
+        renderPopularMovies,
+        renderNewMovies,
+        renderTrendingMovies,
+        renderActionMovies,
+        renderComedyMovies,
+        renderRomanceMovies,
+        renderDramaMovies,
+        renderThrillerMovies,
+        renderAdventureMovies,
+        renderCrimeMovies,
+        renderHorrorMovies } = useContext( UserContext )
 
-    const randomMovieTrailer = Math.floor(Math.random() * movies.length)
 
-    const popularMovies = movies.filter((movie) => movie.popular ? true : false)
-    const renderPopularMovies = popularMovies.map((movie) => (
-        <Movies
-        key={movie.id}
-        movie={movie}
-        />
-    ))
   return (
     <>
     <div className='homepageTrailerDiv'>
@@ -25,7 +30,7 @@ export default function Home() {
         <br/>
         <div>
             <div className='homepageHeaders'>
-                <h1> Check Out { movies[randomMovieTrailer]?.title }</h1>
+                <h1> Check Out <b>{ movies[randomMovieTrailer]?.title }</b></h1>
             </div>
         </div>
         <hr/>
@@ -39,9 +44,130 @@ export default function Home() {
                 <span id="left-arrow" class="arrow">‹</span>
                 <span id="right-arrow" class="arrow">›</span>
                     <div class="slider" id="slider">
-                        {renderPopularMovies}
+                        { renderPopularMovies }
                     </div>
             </section>
+            <br/>
+
+                <div className='homepageHeaders'>
+                    <h1>New Releases On CyberFlix</h1>
+                </div>
+                <section className='homepageSlideContainer'>
+                    <span id="left-arrow" class="arrow">‹</span>
+                    <span id="right-arrow" class="arrow">›</span>
+                        <div class="slider" id="slider">
+                            { renderNewMovies }
+                        </div>
+                </section>
+                <br/>
+
+                <div className='homepageHeaders'>
+                    <h1>Trending Now On CyberFlix</h1>
+                </div>
+                <section className='homepageSlideContainer'>
+                    <span id="left-arrow" class="arrow">‹</span>
+                    <span id="right-arrow" class="arrow">›</span>
+                        <div class="slider" id="slider">
+                            { renderTrendingMovies }
+                        </div>
+                </section>
+                <br/>
+
+                <div className='homepageHeaders'>
+                        <h1>Action</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderActionMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                <div className='homepageHeaders'>
+                        <h1>Comedy</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderComedyMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Romance</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderRomanceMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Drama</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderDramaMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Adventure</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderAdventureMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Thriller</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderThrillerMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Crime</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderCrimeMovies }
+                            </div>
+                    </section>
+                    <br/>
+
+                    <div className='homepageHeaders'>
+                        <h1>Horror</h1>
+                    </div>
+                    <section className='homepageSlideContainer'>
+                        <span id="left-arrow" class="arrow">‹</span>
+                        <span id="right-arrow" class="arrow">›</span>
+                            <div class="slider" id="slider">
+                                { renderHorrorMovies }
+                            </div>
+                    </section>
+                    <br/>
         </div>
     </>
   )
