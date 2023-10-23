a060ae7201a8d9fb07a1397637ffc21fbaba1b67
Justin Bass <justinbass6036@gmail.com>
2023-10-23 15:11:25 -0400

    Created ActionPage to render only action movies. This will allow users to navigate to movies based on genre.

---
+++ client/src/pages/ActionPage.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import { MovieContext } from '../context/movie'
+
+export default function ActionPage() {
+    const { renderActionMovies } = useContext( MovieContext )
+  return (
+    <div>
+        
+    </div>
+  )
+}
