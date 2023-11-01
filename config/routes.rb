e06771b9d45a4c4940b8d3fab9750dd6388783f1
Justin Bass <justinbass6036@gmail.com>
2023-11-01 00:05:03 -0400

    Created a SessionsContoller with a create and destroy action. The create action will allow a user to login into thier account if all credentials match. The destroy action will allow a user to logout of thier account by killing the session. When we kill the session we take the piece of the information the browser needs to authenticate the user. There for the is no way to say the user is who they are. The user is no longer authorized to view certain content. I also registered routes in the routes.rb to map to these actions given the HTTP request sent from the front end. I also added a show action in the UsersController to keep the user logged in as they navigate through the application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post '/signup', to: 'users#create'
- 
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :movies, only: [ :index ]
   resources :actors, only: [ :index ]
