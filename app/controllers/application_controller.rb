99a1d22fd246ce862a520084445bded5a5588b54
Justin Bass <justinbass6036@gmail.com>
2023-10-31 23:44:26 -0400

    Set up private methods in ApplicationController. This includes exception methods utilized by the rescue_from exception method. I set this up in the ApllicationContoller because all other controllers inherit from ApplicationContoller meaning we can raise exceptions globally. If a user enters invalid data an exception will be raised depending which route or HTTP request maps to.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
+  include ActionController::Cookies
 
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
-      end
+  rescue_from ActiveRecord::RecordInvalid, with: :raise_invalid_exception
+  rescue_from ActiveRecord::RecordNotFound, with: :raise_not_found_exception
+  before_action :authorize
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { error: ["You are not authorized to view this page. Please login or signup."] }, status: :unauthorized unless @current_user
+  end
+
+  def raise_invalid_exception(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def raise_not_found_exception
+    render json: {error: ['Not found']}, status: :not_found
+  end
 end
